11cb60275c009b268787da7564a989cb76ba5ae6
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2024-03-28 10:50:33 -0400

    Split and renamed smolceptron module

---
+++ smolceptron.py
@@ -0,0 +1,25 @@
+import csv
+
+class Perceptron:
+    def __init__(self, size: int, learning_rate: float = 0.01):
+        self.learning_rate = learning_rate
+        self.weights = [0] * size
+    
+    def inference(self, input: list):
+        if len(input) != len(self.weights):
+            raise ValueError("Input size must match model size")
+        # Multiply inputs by weights and sum them
+        net = 0
+        for i, value in enumerate(input):
+            net += value * self.weights[i]
+        # Step function
+        if net > 0:
+            return 1
+        else:
+            return -1
+        
+    def update_weights(self, predicted_value: int, actual_value: int, input: list):
+        if len(input) != len(self.weights):
+            raise ValueError("Input size must match model size")
+        for i, value in enumerate(input):
+            self.weights[i] += self.learning_rate * (actual_value - predicted_value) * value
